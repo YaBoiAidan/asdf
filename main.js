eb221e3ecbc0008da88bd1001902d9dd5a383448
YaBoiAidan <91574398+YaBoiAidan@users.noreply.github.com>
2022-01-06 17:12:17 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-song = ""
-song2 = ""
+song1 = "false"
+song2 = "false"
 leftwristX = 0;
 leftwristY = 0;
 rightwristX = 0;
 rightwristY = 0;
+scoreleftWrist = 0;
+scorerightWrist = 0;
 
 function preload()
 {
@@ -24,8 +26,34 @@ posenet.on('pose', gotposes);
 function draw()
 {
     image(video,0,0,600,500);
-}
+    fill("red");
+    stroke("black");
+    if (scoreleftWrist > 0.2) 
+    {
+        circle(leftwristX,leftwristY,30);
+        song2.stop();
+        song2 = "false"
+        if (song = "false") {
+        song.play();
+        song = "playing"
+        document.getElementById("bruh").innerHTML = "Song 1";
+        }
+    }
 
+    fill("red");
+    stroke("black");
+    if (scoreleftWrist > 0.2) 
+    {
+        circle(rightwristX,rightwristY,30);
+        song.stop();
+        song = "false"
+        if (song2 = "false") {
+        song2.play();
+        song2 = "playing"
+        document.getElementById("bruh").innerHTML = "Song 2";
+        }
+    }
+}
 
 function modelloaded()
 {
@@ -42,5 +70,9 @@ if (results.length > 0) {
     rightwristY = results[0].pose.rightWrist.y;
     console.log("leftwristX = "+ leftwristX +" leftwristY = " +leftwristY);
     console.log("rightwristX = "+ rightwristX +" rightwristY = "+rightwristY);
+    scoreleftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreleftWrist = " + scoreleftWrist);
+    scorerightWrist = results[0].pose.keypoints[10].score;
+    console.log("scorerightWrist = " + scorerightWrist);
 }
 }
